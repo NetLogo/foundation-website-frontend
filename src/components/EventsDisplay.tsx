25a3eb2e19e70a017169d578c7ff87db12360d9f
Acero Liang <acero.liangli@stonybrook.edu>
2024-06-21 13:17:57 -0400

    added more

--- src/components/EventsDisplay.tsx
+++ src/components/EventsDisplay.tsx
@@ -16,6 +16,7 @@ interface EventsDisplayProps {
 
 const EventsDisplay = ({ title, events }: EventsDisplayProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showAll, setShowAll] = useState(false);
   
   function formatDate(date: string): string {
       const dateComponents = date.split("-");
@@ -23,27 +24,43 @@ const EventsDisplay = ({ title, events }: EventsDisplayProps) => {
       return `${dateComponents[1]}/${dateComponents[2]}/${dateComponents[0]}`;
   }
 
+  const topThreeEvents = events.slice(0, 3);
+
+  const handleHeaderClick = () => {
+    if (isOpen) {
+      setShowAll(false);
+    }
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="event-display-cont">
       <div
         className={isOpen && events.length > 0 ? "event-display-header event-display-opened" : "event-display-header"}
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={handleHeaderClick}
       >
         <span className="event-display-title">{title}</span>
         <img src={isOpen ? dropDownUp.src : dropDownDown.src} alt="dropdown icon" />
       </div>
-      {isOpen && events.length > 0 && (
+      {isOpen && (
         <div>
-          {events.map((event: Event, index: number) => (
+          {(showAll ? events : topThreeEvents).map((event: Event, index: number) => (
             <div className="event-cont" key={index}>
               <span className="event-date">{formatDate(event.date)}</span>
               <span className="event-title">{event.event_title}</span>
             </div>
           ))}
+          {!showAll && events.length > 3 && (
+            <div className="event-cont event-more" onClick={() => setShowAll(true)}>
+              <span>More</span>
+            </div>
+          )}
         </div>
       )}
     </div>
   );
 };
+
 export { EventsDisplay };
 export type { Event };
+

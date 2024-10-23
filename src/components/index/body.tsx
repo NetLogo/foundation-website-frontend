a23ef89feafbb93e6e0e0f2746270ce196af8fd7
Zyanik <gerardoperez8a@gmail.com>
2024-10-22 19:21:17 -0500

    Adjusted Body Sections

--- src/components/index/body.tsx
+++ src/components/index/body.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 import { Header } from "../layout/header";
 import { Announcement } from "../layout/announcement";
 import type { AnnouncementObj } from "../layout/announcement";
@@ -15,40 +15,47 @@ import type { Event } from "./event-display";
 import "./styles/body.css";
 
 interface BodyProps {
-    announcement?: AnnouncementObj
-    upcomingEvents: Event[];
-    competitions: Event[];
-    upcomingWorkshops: Event[];
-    publications: Event[];
-    communityContent: CommunityPost[];
+  announcement?: AnnouncementObj;
+  upcomingEvents: Event[];
+  competitions: Event[];
+  upcomingWorkshops: Event[];
+  publications: Event[];
+  communityContent: CommunityPost[];
 }
 
-function Body({announcement, upcomingEvents, upcomingWorkshops, competitions, publications, communityContent} : BodyProps) {
-    const [showAnnouncement, setShowAnnouncement] = useState(!!announcement);
-    return (
-        <div className="body">
-            <Header/>
+function Body({
+  announcement,
+  upcomingEvents,
+  upcomingWorkshops,
+  competitions,
+  publications,
+  communityContent,
+}: BodyProps) {
+  const [showAnnouncement, setShowAnnouncement] = useState(!!announcement);
 
-            { showAnnouncement && announcement && ( 
-                <Announcement 
-                announcement={announcement}
-                setShowAnnouncement={setShowAnnouncement}
-                />
-            )}
-            <Intro/>
-            <WhyNetLogo/>
-            <GetNetLogo/>
-            <Community
-            communityPosts={communityContent}/>
-            <News upcomingEvents={upcomingEvents}
+  return (
+    <div className="body">
+      <Header />
+
+      {showAnnouncement && announcement && (
+        <Announcement
+          announcement={announcement}
+          setShowAnnouncement={setShowAnnouncement}
+        />
+      )}
+      <Intro />
+      <WhyNetLogo />
+      <GetNetLogo />
+      <Community communityPosts={communityContent} />
+      {/* <News upcomingEvents={upcomingEvents}
             upcomingWorkshops={upcomingWorkshops}
             competitions={competitions}
-            publications={publications}/>
-            <FeaturedPartners/>
-            <MailingList/>
-            <Footer/>
-        </div>
-    )
+            publications={publications}/> */}
+      {/* <FeaturedPartners/> */}
+      <MailingList />
+      <Footer />
+    </div>
+  );
 }
 
 export { Body };

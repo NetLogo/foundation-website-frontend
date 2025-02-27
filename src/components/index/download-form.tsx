547af7555bd82d489ba5e513145867d43ceda34f
Zyanik <gerardoperez8a@gmail.com>
2025-02-26 23:48:57 -0600

    Added all refs

--- src/components/index/download-form.tsx
+++ src/components/index/download-form.tsx
@@ -4,15 +4,27 @@ import "./styles/download-form.css";
 const DownloadForm = () => {
   return (
     <div className="download-form">
-      <h1>Download NetLogo</h1>
+      <h1 className="form-title">Download NetLogo</h1>
       <p>
+        {"Most computers can run NetLogo (see "}
+        <a
+          target="_blank"
+          className="form-ref"
+          href="https://ccl.northwestern.edu/netlogo/requirements.html"
+        >
+          system requirements
+        </a>
         {
-          "Most computers can run NetLogo (see system requirements). If you wouldlike to run NetLogo on a Chromebook or in a web browser, please see if NetLogo Web will meet your needs."
+          "). If you would like to run NetLogo on a Chromebook or in a web browser, please see if "
         }
+        <a className="form-ref" target="_blank" href="http://netlogoweb.org/">
+          NetLogo Web
+        </a>
+        {" will meet your needs."}
       </p>
       <p>
         {
-          "Multiple versions of NetLogo can be installed on the same computer; installing a new one doesnt remove the old one. "
+          "Multiple versions of NetLogo can be installed on the same computer; installing a new one doesn't remove the old one. "
         }
       </p>
       <form>
@@ -33,8 +45,26 @@ const DownloadForm = () => {
           </div>
         </div>
         <div className="detail-row">
-          <p>More versions here</p>
-          <p>For help using old models with new versions see the Transition Guide.</p>
+          <p>
+            {"More versions "}
+            <a
+              target="_blank"
+              className="form-ref"
+              href="https://ccl.northwestern.edu/netlogo/oldversions.shtml"
+            >
+              {"here"}
+            </a>
+            <br />
+            {"For help using old models with new versions see the "}
+            <a
+              target="_blank"
+              className="form-ref"
+              href="https://ccl.northwestern.edu/netlogo/transition.html"
+            >
+              {"Transition Guide"}
+            </a>
+            .
+          </p>
         </div>
         <div className="form-row">
           <label htmlFor="name">Name</label>
@@ -48,19 +78,39 @@ const DownloadForm = () => {
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
         </div>
-        <div className="detail-row">
-            <input type="checkbox" id="subscribe" name="subscribe" />
-            <label htmlFor="subscribe">Update me on the newest releases of NetLogo</label>
+        <div className="detail-row email-row">
+          <input
+            className="email-signup"
+            type="checkbox"
+            id="subscribe"
+            name="subscribe"
+          />
+          <label htmlFor="subscribe">
+            Update me on the newest releases of NetLogo
+          </label>
         </div>
         <div className="form-row">
           <label htmlFor="comments">Comments</label>
           <textarea id="comments" name="comments" rows={4} />
         </div>
         <div className="detail-row">
-          <p>We read these but dont respond directly. For a response, write feedback@ccl.northwestern.edu.</p>
+          <p>
+            {"We read these but don't respond directly. For a response, write "}
+            <a className="form-ref" href="mailto:feedback@ccl.northwestern.edu">
+              {"feedback@ccl.northwestern.edu."}
+            </a>
+          </p>
         </div>
         <div className="submit-row">
           <button type="submit">Download</button>
+        </div>
+        <div className="detail-row">
+          <p>
+            {"Download trouble? Write"}{" "}
+            <a className="form-ref" href="mailto:bugs@ccl.northwestern.edu">
+              {"bugs@ccl.northwestern.edu."}
+            </a>
+          </p>
         </div>
       </form>
     </div>

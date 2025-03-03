769984dbb60e2ee415a5cc14b9eef3c3673b8aa5
Zyanik <gerardoperez8a@gmail.com>
2025-03-03 15:54:04 -0600

    Responsive design for donload page

--- src/components/index/download-page.tsx
+++ src/components/index/download-page.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/download-page.css";
 import { DownloadForm } from "./download-form";
 import NetLogoIcon from "../../assets/NetlogoIcon.svg";
 
 const DownloadSection = () => {
+  const [isDesktop, setDesktop] = useState(false); // Default to false initially
+
+  useEffect(() => {
+    // Set initial value once in browser
+    setDesktop(window.innerWidth > 1300);
+    
+    const updateMedia = () => {
+      setDesktop(window.innerWidth > 1300);
+    };
+
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
   return (
     <div className="download-section">
-      <img className="netlogo-download-icon" src={NetLogoIcon.src} />
+      {isDesktop && <img className="netlogo-download-icon" src={NetLogoIcon.src} />}
       <DownloadForm />
     </div>
   );

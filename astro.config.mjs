4fe9f26c74cf61c50b949dff5dc7e56b875253df
Gerardo Perez <106106326+geperez8@users.noreply.github.com>
2025-05-06 10:43:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [react()],
-  site: 'https://netlogo.github.io/',
-  base: '/foundation-website-frontend'
+  site: 'https://www.netlogo.org/',
 });

391a93e052fa5f3933c56c4a673bc9fab1656b69
danielwong0115 <daniellwong442@gmail.com>
2025-06-26 13:00:23 -0500

    Fix layout of references page

---
+++ src/components/announcements/announcements-page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AnnouncementsSection.css";
+
+
+const AnnouncementsSection = () => {
+    return (
+        <div className="contacts-container">
+            <h1>Announcements</h1>
+        </div>
+    );
+};
+
+export { AnnouncementsSection };
---
+++ src/components/contact/contact-page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ContactSection.css";
+
+
+const ContactSection = () => {
+    return (
+        <div className="contacts-container">
+            <h1>Contact</h1>
+        </div>
+    );
+};
+
+export { ContactSection };

719fdfe779fe7ea8aaebc697a6f199f71f11c181
danielwong0115 <daniellwong442@gmail.com>
2025-07-23 16:01:48 -0500

    Allow donate button to stay on upper right of navbar most of the time

--- src/components/layout/header2.tsx
+++ src/components/layout/header2.tsx
@@ -20,16 +20,49 @@ if (window.location.pathname !== homePath) {
 const Header = ({ navData }: HeaderProps) => {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap.bundle.min.js');
+
+    const button = document.getElementById("toggleButton");
+    const navOptions = document.getElementById("navbarContent");
+    const donateButton = document.getElementById("donationButton");
+
+    if (!button || !navOptions || !donateButton) return;
+
+    const handleClick = () => {
+      const isExpanded = button.getAttribute("aria-expanded") === "true";
+
+      button.setAttribute("aria-expanded", String(!isExpanded));
+
+      if (isExpanded) {
+        navOptions.classList.remove("order-2");
+        donateButton.classList.remove("order-3");
+        donateButton.classList.add("order-2");
+        navOptions.classList.add("order-3");
+      } else if (!isExpanded) {
+        navOptions.classList.remove("order-3");
+        donateButton.classList.remove("order-2");
+        donateButton.classList.add("order-3");
+        navOptions.classList.add("order-2");
+      }
+    };
+
+    button.addEventListener("click", handleClick);
+
+    return () => {
+      button.removeEventListener("click", handleClick);
+    };
   }, []);
 
   return (
+
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom font-inter">
-      <div className="container d-flex justify-content-between align-items-center">
-      
-        <div className="d-flex align-items-center">
+      <div className="container d-flex align-items-center justify-content-between">
+        
+        {/* this is combining the burger button first then the netlogo icon */}
+        <div className="d-flex align-items-center order-1">
           <button
-            className="navbar-toggler order-1 order-lg-2 me-2"
+            className="navbar-toggler me-2"
             type="button"
+            id="toggleButton"
             data-bs-toggle="collapse"
             data-bs-target="#navbarContent"
             aria-controls="navbarContent"
@@ -39,12 +72,14 @@ const Header = ({ navData }: HeaderProps) => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <a className="navbar-brand fw-bold order-2 order-lg-1" href="/">
+          <a className="navbar-brand fw-bold" href="/">
             <img src={NetLogoOrgLogo.src} alt="NetLogo Logo" width="175" />
           </a>
         </div>
-        <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav mx-auto d-flex justify-content-between">
+        
+        {/* then directus content should vary based off of aria-expanded */}
+        <div className="collapse navbar-collapse justify-content-center order-2" id="navbarContent">
+          <ul className="navbar-nav">
             {navData.map((section, i) => (
               <li key={i} className="nav-item mx-5 dropdown">
                 <a
@@ -63,17 +98,22 @@ const Header = ({ navData }: HeaderProps) => {
                         {item.display_title}
                       </a>
                     </li>
-                  ))}
+                  ))} 
                 </ul>
               </li>
             ))}
           </ul>
         </div>
-        <a className="btn btn-primary ms-2" onClick={NavigateToDonate} role="button">
-          Donate
-        </a>
+        {/* then donate button should also be varying based also opff of aria-expanded */}
+        <div className="d-flex align-items-center order-3" id="donationButton">
+          <a className="btn btn-primary ms-auto me-2" onClick={NavigateToDonate} role="button">
+            Donate
+          </a>
+        </div>
       </div>
+
     </nav>
+
   )
 };
 

0630b7856f7d63c4121c143bf192a42efe1a4449
danielwong0115 <daniellwong442@gmail.com>
2025-07-24 11:31:20 -0500

    Fix placement of donate button without javascript

--- src/components/layout/header2.tsx
+++ src/components/layout/header2.tsx
@@ -21,48 +21,15 @@ const Header = ({ navData }: HeaderProps) => {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap.bundle.min.js');
 
-    const button = document.getElementById("toggleButton");
-    const navOptions = document.getElementById("navbarContent");
-    const donateButton = document.getElementById("donationButton");
-
-    if (!button || !navOptions || !donateButton) return;
-
-    const handleClick = () => {
-      const isExpanded = button.getAttribute("aria-expanded") === "true";
-
-      button.setAttribute("aria-expanded", String(!isExpanded));
-
-      if (isExpanded) {
-        navOptions.classList.remove("order-2");
-        donateButton.classList.remove("order-3");
-        donateButton.classList.add("order-2");
-        navOptions.classList.add("order-3");
-      } else if (!isExpanded) {
-        navOptions.classList.remove("order-3");
-        donateButton.classList.remove("order-2");
-        donateButton.classList.add("order-3");
-        navOptions.classList.add("order-2");
-      }
-    };
-
-    button.addEventListener("click", handleClick);
-
-    return () => {
-      button.removeEventListener("click", handleClick);
-    };
   }, []);
 
   return (
-
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom font-inter">
-      <div className="container d-flex align-items-center justify-content-between">
-        
-        {/* this is combining the burger button first then the netlogo icon */}
-        <div className="d-flex align-items-center order-1">
+      <div className="container d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
           <button
-            className="navbar-toggler me-2"
+            className="navbar-toggler order-1 order-lg-2 me-2"
             type="button"
-            id="toggleButton"
             data-bs-toggle="collapse"
             data-bs-target="#navbarContent"
             aria-controls="navbarContent"
@@ -72,14 +39,15 @@ const Header = ({ navData }: HeaderProps) => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <a className="navbar-brand fw-bold" href="/">
+          <a className="navbar-brand fw-bold order-2 order-lg-1" href="/">
             <img src={NetLogoOrgLogo.src} alt="NetLogo Logo" width="175" />
           </a>
         </div>
-        
-        {/* then directus content should vary based off of aria-expanded */}
-        <div className="collapse navbar-collapse justify-content-center order-2" id="navbarContent">
-          <ul className="navbar-nav">
+        <a className="d-lg-none btn btn-primary" onClick={NavigateToDonate} role="button">
+            Donate
+        </a>
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav mx-auto d-flex justify-content-between">
             {navData.map((section, i) => (
               <li key={i} className="nav-item mx-5 dropdown">
                 <a
@@ -93,27 +61,22 @@ const Header = ({ navData }: HeaderProps) => {
                 </a>
                 <ul className="dropdown-menu">
                   {section.items.map((item, j) => (
-                    <li key={`${j}`}>
+                    <li key={`item-${j}`}>
                       <a className="dropdown-item header-link long-item" href={item.url}>
                         {item.display_title}
                       </a>
                     </li>
-                  ))} 
+                  ))}
                 </ul>
               </li>
             ))}
           </ul>
         </div>
-        {/* then donate button should also be varying based also opff of aria-expanded */}
-        <div className="d-flex align-items-center order-3" id="donationButton">
-          <a className="btn btn-primary ms-auto me-2" onClick={NavigateToDonate} role="button">
-            Donate
-          </a>
-        </div>
+        <a className="d-none d-lg-block btn btn-primary ms-2 ms-auto" onClick={NavigateToDonate} role="button">
+          Donate
+        </a>
       </div>
-
     </nav>
-
   )
 };
 

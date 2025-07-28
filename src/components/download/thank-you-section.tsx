97b1e17f51afbfb27bcf844c8a93b6f5faad4c84
danielwong0115 <daniellwong442@gmail.com>
2025-07-28 13:30:37 -0500

    Add text with correct link that allows user to download again

--- src/components/download/thank-you-section.tsx
+++ src/components/download/thank-you-section.tsx
@@ -1,22 +1,22 @@
 import "./styles/download-section.css";
 import ContentImageLayout from "../shared/content-image-layout";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const ThankYouSection = () => {
-    
+
+    // const [downloadinURL, setDownloadinURL] = useState(false);
+    const [downloadValue,  setdownloadValue] = useState<string | null>(null);
+
     useEffect(() => {
-        // console.log("yo...");
-        // console.log("window.location.search:", window.location.search)
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
-        const downloadValue = urlParams.get("download_url");
+        setdownloadValue(urlParams.get("download_url"));
+        }, []);
 
-        // console.log("query string:", queryString);
-        // console.log("Raw download cal:", downloadValue);
-        
+    useEffect(() => {
         if (downloadValue) {
             try {
-                // console.log("hiiiiiii")
+                // setDownloadinURL(true);
                 const link = document.createElement("a");
                 link.href = atob(downloadValue);
                 link.download = "";
@@ -27,12 +27,32 @@ const ThankYouSection = () => {
             catch (error) {
                 console.error("Invalid download URL:", error);
             }}
-        }, []);
+
+        else {
+            return;
+        }
+        }, [downloadValue]);
+
+    const DownloadAgain = () => {
+        if (downloadValue) {
+        return (
+            <p className="text-start pt-5">If your download didn't work,{" "}<a href={atob(downloadValue)}>click here</a></p>
+        );
+        } else {
+        return (
+            <p className="text-start pt-5">You may not have downloaded before coming to this page.{" "}<a href="/download">Click here</a>{" "}
+            to go to the download page.</p>
+        )
+        }
+    };
+
+    
 
     return (
         <ContentImageLayout>
-            <div className="thankyou-container">
+            <div className="thankyou-container font-inter">
                 <h1 className="d-flex flex-row col-12 text-nowrap fw-semibold">Thanks for downloading!</h1>
+                <DownloadAgain />
             </div>
         </ContentImageLayout>
     );

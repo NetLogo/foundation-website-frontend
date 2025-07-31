780e5d37203309181f19b6743cee74b36ff2a7ac
danielwong0115 <daniellwong442@gmail.com>
2025-07-31 13:03:39 -0500

    About page markdown rendered images centered

--- src/components/about/about-page.tsx
+++ src/components/about/about-page.tsx
@@ -12,12 +12,24 @@ interface AboutSectionProps {
 
 const AboutSection = ({ AboutData }: AboutSectionProps) => {
     return(
-        <div className='text-start pt-4 px-5'>
+        <div className='text-start pt-4 px-5 ms-auto me-auto w-75'>
             {AboutData.length === 0 ? (
                 <p>No about content available.</p>
             ) : (
                 AboutData.map((entry, index) => (
-                    <ReactMarkdown key={index}>
+                    <ReactMarkdown 
+                        key={index}
+                        components={{
+                        img: ({ node, ...props }) => (
+                            <div className="text-center pt-3">
+                            <img
+                                {...props}
+                                className='rounded'
+                                alt={props.alt || ''}
+                            />
+                            </div>
+                        ),
+                        }}>
                         {entry.body}
                     </ReactMarkdown>
             ))

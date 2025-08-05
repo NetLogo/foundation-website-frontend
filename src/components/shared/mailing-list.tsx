8279beb1a19f5e5352dfb586e22b609d3c9711ac
danielwong0115 <daniellwong442@gmail.com>
2025-08-05 17:04:06 -0500

    Add responsive styling for mailing list component on intro page

--- src/components/shared/mailing-list.tsx
+++ src/components/shared/mailing-list.tsx
@@ -13,12 +13,12 @@ const MailingList = () => {
 
     return(
         <div className="container w-100 py-5 font-inter" id="mailing-container">
-            <div className="row">
-                <div className="col-lg-6 ps-5">
-                    <h1 className="text-start fw-bold">Join our mailing list!</h1>
+            <div className="row justify-content-center align-items-center">
+                <div className="col-lg-6 ps-5 pb-3 mx-auto">
+                    <h1 className="fw-bold text-start">Join our mailing list!</h1>
                     <p className="fs-6 fw-medium description-text text-start">Join the NetLogo mailing list to keep up-to-date with what's happening in the NetLogo Community!</p>
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-6 d-flex flex-column align-items-center align-items-lg-start">
                     <input type="text" className="form-control w-75 mb-3" placeholder="First Name"></input>
                     <input type="text" className="form-control w-75 mb-3" placeholder="Last Name"></input>
                     <input type="email" className="form-control w-75 mb-3" placeholder="Email"></input>

9376d4144f3bc2564bc6747b51859de887517f84
danielwong0115 <daniellwong442@gmail.com>
2025-08-15 18:03:33 -0500

    Unnecessary importing of bsky module because it is in layout.astro already

--- src/components/index/newsfeed.tsx
+++ src/components/index/newsfeed.tsx
@@ -2,14 +2,11 @@ import { Section } from "../shared/section";
 import ReactMarkdown from 'react-markdown';
 
 
-
 const Newsfeed = () => {
     return (
         <div className="container-fluid text-start font-inter" style={{backgroundColor:"white", paddingLeft: "10rem"}}>
             <h1 className="fw-bold" style={{fontSize:"3rem"}}>News</h1>
             <p style={{fontSize:"18px"}}>Learn about latest news and upcoming events in NetLogo community.</p>
-
-            <script type="module" src="https://cdn.jsdelivr.net/npm/bsky-embed/dist/bsky-embed.es.js" async></script>
 
             <bsky-embed
                 username="netlogo.bsky.social"

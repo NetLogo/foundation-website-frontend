c891544bcc35c9053c9864391d4bc91b320ba006
danielwong0115 <daniellwong442@gmail.com>
2025-08-20 14:13:53 -0500

    Fix grammatical issue on download page

--- src/components/download/download-machine-help.tsx
+++ src/components/download/download-machine-help.tsx
@@ -9,11 +9,11 @@
             here</a>.
             </p>
             <p className="pt-3 font-inter">
-            NetLogo also works for{" "}
+            NetLogo works for{" "}
             <a href={`/downloads/${otherplatform1.toLowerCase()}`} className="text-decoration-none">{otherplatform1}</a>
             {" "} and {" "}
             <a href={`/downloads/${otherplatform2.toLowerCase()}`} className="text-decoration-none">{otherplatform2}</a>
-            {" "} too.
+            , too.
             </p>
         </div>
     );
